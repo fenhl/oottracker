fff6badf245c22ba7e6db26aad2bd78d8eb52ea3
Fenhl <fenhl@fenhl.net>
2020-11-11 05:52:40 +0000

    Make sure oottracker-bizhawk build script is always run

--- crate/oottracker-bizhawk/build.rs
+++ crate/oottracker-bizhawk/build.rs
@@ -6,6 +6,7 @@ use std::{
 };
 
 fn main() -> io::Result<()> {
+    println!("cargo:rerun-if-changed=nonexistent.foo"); // check a nonexistent file to make sure build script is always run (see https://github.com/rust-lang/cargo/issues/4213 and https://github.com/rust-lang/cargo/issues/5663)
     let source_path = match &env::var("PROFILE").expect("missing PROFILE envar")[..] {
         "debug" => Path::new("../../target/debug/oottracker.dll"),
         "release" => Path::new("../../target/release/oottracker.dll"),
