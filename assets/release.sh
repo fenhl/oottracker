a7ff135c690475353d3d2efe2cb25fcdceccc794
Fenhl <fenhl@fenhl.net>
2021-02-17 14:42:34 +0000

    Fix release script on macOS

--- assets/release.sh
+++ assets/release.sh
@@ -15,11 +15,31 @@ for arg in "$@"; do
     esac
 done
 
-set -e
-
 # make sure everything is executed relative to this script's location
 cd "${0:a:h}"/..
 
-/opt/git/github.com/fenhl/syncbin/master/bin/rust $quiet --no-project
-git pull $quiet_verbose --ff-only
-cargo $quiet_verbose run --release --package=oottracker-utils --bin=oottracker-release -- $verbose
+function lock {
+    echo 'acquiring rust lockdir'
+    until mkdir /tmp/syncbin-startup-rust.lock &> /dev/null; do
+        if [[ -f /tmp/syncbin-startup-rust.lock/pid ]] && ! ps -p "$(cat /tmp/syncbin-startup-rust.lock/pid)" &> /dev/null; then
+            unlock
+        fi
+        sleep 1
+    done
+    trap 'rm -rf /tmp/syncbin-startup-rust.lock' HUP TERM INT # remove lock when script finishes
+    echo $$ > "/tmp/syncbin-startup-rust.lock/pid"
+}
+
+function unlock {
+    rm -f /tmp/syncbin-startup-rust.lock/pid # remove pidfile, if any
+    rmdir /tmp/syncbin-startup-rust.lock # remove lock
+    trap ':' HUP TERM INT # neutralize trap
+}
+
+# make sure multiple instances of rustup aren't running at the same time
+lock
+rustup $quiet_verbose update stable || exit $?
+unlock
+
+git pull $quiet_verbose --ff-only || exit $?
+cargo $quiet_verbose run --release --package=oottracker-utils --bin=oottracker-release -- $verbose || exit $?
