10e6945b8a48fca670a6e7b23eeed0bd67877854
Fenhl <fenhl@fenhl.net>
2021-08-19 14:03:58 +0000

    Finish version-bump utility

--- crate/oottracker-utils/src/version.rs
+++ crate/oottracker-utils/src/version.rs
@@ -2,26 +2,36 @@ use {
     std::process::Stdio,
     itertools::Itertools as _,
     semver::Version,
-    serde::Deserialize,
+    serde::{
+        Deserialize,
+        Serialize,
+    },
+    serde_json::Value as Json,
     tokio::process::Command,
 };
 
-#[derive(Deserialize)]
-struct Plist {
+pub(crate) const INFO_PLIST_PATH: &str = "assets/macos/OoT Tracker.app/Contents/Info.plist";
+
+#[derive(Deserialize, Serialize)]
+pub(crate) struct Plist {
     #[serde(rename = "CFBundleShortVersionString")]
-    bundle_short_version_string: Version,
+    pub(crate) bundle_short_version_string: Version,
+    #[serde(rename = "CFBundleVersion")]
+    pub(crate) bundle_version: String,
+    #[serde(flatten)]
+    _rest: Json, //HACK see https://github.com/ebarnard/rust-plist/issues/54#issuecomment-653756460
 }
 
-pub async fn check_cli_version(package: &str, version: &Version) {
+pub(crate) async fn check_cli_version(package: &str, version: &Version) {
     let cli_output = String::from_utf8(Command::new("cargo").arg("run").arg(format!("--package={}", package)).arg("--").arg("--version").stdout(Stdio::piped()).output().await.expect("failed to run CLI with --version").stdout).expect("CLI version output is invalid UTF-8");
     let (cli_name, cli_version) = cli_output.split(' ').collect_tuple().expect("no space in CLI version output");
     assert_eq!(cli_name, package);
     assert_eq!(*version, cli_version.parse().expect("failed to parse CLI version"));
 }
 
-pub async fn version() -> Version {
+pub(crate) async fn version() -> Version {
     let version = Version::parse(env!("CARGO_PKG_VERSION")).expect("failed to parse current version");
-    assert_eq!(version, plist::from_file::<_, Plist>("assets/macos/OoT Tracker.app/Contents/Info.plist").expect("failed to read plist for version check").bundle_short_version_string);
+    assert_eq!(version, plist::from_file::<_, Plist>(INFO_PLIST_PATH).expect("failed to read plist for version check").bundle_short_version_string);
     assert_eq!(version, ootr::version());
     assert_eq!(version, ootr_dynamic::version());
     assert_eq!(version, ootr_static::version()); // also checks ootr-static-derive
--- crate/oottracker-utils/src/version_bump.rs
+++ crate/oottracker-utils/src/version_bump.rs
@@ -1,5 +1,19 @@
+#![deny(rust_2018_idioms, unused, unused_import_braces, unused_lifetimes, unused_qualifications, warnings)]
+#![forbid(unsafe_code)]
+
 use {
+    std::{
+        fmt,
+        num::ParseIntError,
+    },
+    derive_more::From,
+    semver::Version,
     structopt::StructOpt,
+    tokio::{
+        fs,
+        io,
+    },
+    toml_edit::TomlError,
     crate::version::version,
 };
 
@@ -10,15 +24,54 @@ enum Args {
     Major,
     Minor,
     Patch,
+    Exact {
+        version: Version,
+    },
+}
+
+#[derive(From)]
+enum Error {
+    Io(io::Error),
+    MissingPackageEntry,
+    PackageIsNotTable,
+    ParseInt(ParseIntError),
+    Plist(plist::Error),
+    Toml(TomlError),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::Io(e) => write!(f, "I/O error: {}", e),
+            Error::MissingPackageEntry => write!(f, "found Cargo manifest without “package” entry"),
+            Error::PackageIsNotTable => write!(f, "“package” entry in Cargo manifest was not a table"),
+            Error::ParseInt(e) => e.fmt(f),
+            Error::Plist(e) => write!(f, "plist error: {}", e),
+            Error::Toml(e) => e.fmt(f),
+        }
+    }
 }
 
 #[wheel::main]
-async fn main(args: Args) {
-    let mut version = version().await;
-    match args {
-        Args::Major => version.increment_major(),
-        Args::Minor => version.increment_minor(),
-        Args::Patch => version.increment_patch(),
+async fn main(args: Args) -> Result<(), Error> {
+    let version = match args {
+        Args::Major => { let mut version = version().await; version.increment_major(); version }
+        Args::Minor => { let mut version = version().await; version.increment_minor(); version }
+        Args::Patch => { let mut version = version().await; version.increment_patch(); version }
+        Args::Exact { version } => version,
+    };
+    println!("new version: {}", version);
+    let mut info_plist = plist::from_file::<_, version::Plist>(version::INFO_PLIST_PATH)?;
+    info_plist.bundle_version = (info_plist.bundle_version.parse::<u64>()? + 1).to_string();
+    info_plist.bundle_short_version_string = version.clone();
+    plist::to_file_xml(version::INFO_PLIST_PATH, &info_plist)?;
+    let mut crates = fs::read_dir("crate").await?;
+    while let Some(entry) = crates.next_entry().await? {
+        let manifest_path = entry.path().join("Cargo.toml");
+        let mut manifest = fs::read_to_string(&manifest_path).await?.parse::<toml_edit::Document>()?;
+        *manifest.as_table_mut().get_mut("package").ok_or(Error::MissingPackageEntry)?.as_table_mut().ok_or(Error::PackageIsNotTable)?.entry("version")
+            = toml_edit::Item::Value(toml_edit::decorated(version.to_string().into(), " ", ""));
+        fs::write(manifest_path, manifest.to_string().into_bytes()).await?;
     }
-    println!("new version: {}", version); //TODO edit Cargo.toml and Info.plist files
+    Ok(())
 }
