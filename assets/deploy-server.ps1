1b5832543abd355cb88657287d764cdc60d0216a
Fenhl <fenhl@fenhl.net>
2023-10-26 15:16:53 +0000

    Update working dir for web branch

--- assets/deploy-server.ps1
+++ assets/deploy-server.ps1
@@ -10,10 +10,10 @@ $env:PYO3_PYTHON = "python"
 git push
 ThrowOnNativeFailure
 
-ssh fenhl.net 'cd /opt/git/github.com/fenhl/oottracker/main && git pull --ff-only'
+ssh fenhl.net 'cd /opt/git/github.com/fenhl/oottracker/branch/web && git pull --ff-only'
 ThrowOnNativeFailure
 
-ssh fenhl.net "env -C /opt/git/github.com/fenhl/oottracker/main $(Get-Content .\assets\web\env.txt) cargo build --release --package=oottracker-web"
+ssh fenhl.net "env -C /opt/git/github.com/fenhl/oottracker/branch/web $(Get-Content .\assets\web\env.txt) cargo build --release --package=oottracker-web"
 ThrowOnNativeFailure
 
 ssh fenhl.net 'sudo systemctl restart oottracker-web'
